bcc62281f577b09750abd351016ac5d3aed1c346
Admin <admin@Michaels-MacBook-Pro.local>
2020-07-02 16:09:00 +0200

    add hamburger menu for mobile to each page

--- js/cart-qty.js
+++ js/cart-qty.js
@@ -10,24 +10,30 @@ function retrieveFromLocal () {
 
 function cartNavQty () {
   const cartQty = document.querySelector('.cart-qty');
+  const hamCartQty = document.querySelector('.ham-cart-qty');
   const cartItems = retrieveFromLocal();
   if (cartItems.length === 0) { 
     cartQty.innerText = ' ';
   } else { 
     cartQty.innerText = cartItems.length;
+    hamCartQty.innerText = cartItems.length;
   }
 }
 
 document.addEventListener('DOMContentLoaded', cartNavQty)
 
+
+// HAMBURGER MENU FUNCTIONALITY 
+
 const hamburger = document.querySelector('.fa-bars');
 const navMenu = document.querySelector('.navbar');
-const hamNav = document.querySelector('.ham-nav-contain')
+const hamNav = document.querySelector('.ham-nav-container');
 
 document.addEventListener('click', (e) => {
   if (e.target.className.includes('fas')) {
     hamNav.style.display = 'flex';
     hamburger.style.display = 'none';
+    cartNavQty();
   } else if (e.target.className.includes('close')) {
     hamNav.style.display = 'none';
     hamburger.style.display = 'flex';

6552b2c01a22951e563f2240f787ec9b40d67951
Admin <admin@Michaels-MacBook-Pro.local>
2020-08-19 07:22:01 +0200

    Create partials for navbar & footer.

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
-
+const ejs = require('ejs');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+
+
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
